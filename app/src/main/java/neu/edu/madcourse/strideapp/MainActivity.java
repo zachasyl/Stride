8d875a061314908b7e9728512c768a2fc4d3342b
zachasyl <sylvane.z@northeastern.edu>
2022-04-12 03:19:38 -0400

    start run button

--- app/src/main/java/neu/edu/madcourse/strideapp/MainActivity.java
+++ app/src/main/java/neu/edu/madcourse/strideapp/MainActivity.java
@@ -7,11 +7,6 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     @Override
