dfd93c3c4605f61ab06fbbf195ce57668f2bb8c6
zachasyl <sylvane.z@northeastern.edu>
2022-04-25 15:00:32 -0400

    manually entered strides now have unique ids:

--- app/src/main/java/neu/edu/madcourse/strideapp/Sprint.java
+++ app/src/main/java/neu/edu/madcourse/strideapp/Sprint.java
@@ -7,6 +7,7 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Sprint extends AppCompatActivity {
@@ -24,15 +25,13 @@ public class Sprint extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sprint);
         dateEntry = (EditText)findViewById(R.id.dateEntry);
-        idEntry = (EditText)findViewById(R.id.IDentry);
-        distanceEntry = (EditText)findViewById(R.id.IDentry);
+        distanceEntry = (EditText)findViewById(R.id.distanceEntry);
         timeEntry = (EditText)findViewById(R.id.timeEntry);
 
         addButton = findViewById(R.id.floatingActionButton2);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String id = idEntry.getText().toString();
                 String date = dateEntry.getText().toString();
                 String distance = distanceEntry.getText().toString();
                 String time = timeEntry.getText().toString();
@@ -40,14 +39,21 @@ public class Sprint extends AppCompatActivity {
                 String speed =  distance + time;
                 String caloriesBurned = "500";
 
-                        FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("date").setValue(date);
-                        FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("distance").setValue(distance);
-                        FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("time").setValue(time);
+                DatabaseReference x = FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).push();
+                x.child("time").setValue(time);
+                x.child("distance").setValue(distance);
+                x.child("speed").setValue(speed);
+                x.child("date").setValue(date);
+                x.child("calories").setValue(caloriesBurned);
 
-                        FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("speed").setValue(speed);
+                       // FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("date").setValue(date);
+                        //FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("distance").setValue(distance);
+                       // FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("time").setValue(time);
+
+                        //FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("speed").setValue(speed);
 
 
-                        FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("calories").setValue(caloriesBurned);
+                       // FirebaseDatabase.getInstance().getReference().child(String.valueOf("Zach")).child(id).child("calories").setValue(caloriesBurned);
 
 
             }
