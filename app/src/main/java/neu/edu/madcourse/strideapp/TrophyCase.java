3a24dd75b7e831c74f9ffc5523665b9f9d731196
zachasyl <sylvane.z@northeastern.edu>
2022-05-01 22:14:45 -0400

    now checks the database to see if the trophies are earned

--- app/src/main/java/neu/edu/madcourse/strideapp/TrophyCase.java
+++ app/src/main/java/neu/edu/madcourse/strideapp/TrophyCase.java
@@ -1,6 +1,8 @@
 package neu.edu.madcourse.strideapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.installations.FirebaseInstallations;
 
 import java.util.ArrayList;
 
@@ -22,53 +25,62 @@ public class TrophyCase extends AppCompatActivity {
     DatabaseReference database;
     ArrayList<Exercise> list;
     private ImageView mImageView;
+    private String firebaseIdentifier;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         String TotalCalories = "TotalCalories";
-        DatabaseReference ref = database.getReference().child("TotalCalories");
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.trophies);
+
+
+        FirebaseInstallations.getInstance().getId().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                firebaseIdentifier = task.getResult();
+                DatabaseReference ref = database.getReference().child(firebaseIdentifier).child("Trophies");
+
+
 
         ref.addValueEventListener(new ValueEventListener(){
             private ImageView mImageView;
             @Override
 
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-//
-
                     if (snapshot.getValue() != null) {
-                        String ma = snapshot.getValue().toString();
-                        int k = Integer.parseInt(ma);
+                        try {
+
+                            boolean bronze = (boolean) snapshot.child("Bronze").getValue();
+
+                            if (bronze) {
+                                mImageView = (ImageView) findViewById(R.id.bronze);
+                                mImageView.setImageResource(R.drawable.bronze);
+                            }
+
+                            boolean silver = (boolean) snapshot.child("Silver").getValue();
 
 
-                        if (k >= 1500) {
-                            mImageView = (ImageView) findViewById(R.id.imageView3);
-                            mImageView.setImageResource(R.drawable.ic_launcher_foreground);
-                            unlocked = true;
+                            if (silver) {
+                                mImageView = (ImageView) findViewById(R.id.silver);
+                                mImageView.setImageResource(R.drawable.silver);
+                            }
 
-//                            add trophy to database
+                            boolean gold = (boolean) snapshot.child("Gold").getValue();
 
+                            if (gold) {
+                                mImageView = (ImageView) findViewById(R.id.gold);
+                                mImageView.setImageResource(R.drawable.gold);
+
+
+                            }
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
-
-
                     }
 
-
-
-
-//                }
-
-
-
-
             }
-
-
-
-
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -76,7 +88,39 @@ public class TrophyCase extends AppCompatActivity {
             }
         });
 
+            }});
+    }
 
+    public void onClickManual(View v) {
+
+        Intent Sprint = new Intent(TrophyCase.this, Sprint.class);
+        startActivity(Sprint);
+
+    }
+
+    public void onClickMyStats(View v) {
+        // go to the activity for displaying journeys
+        Intent MyStats = new Intent(TrophyCase.this, Statistics.class);
+        startActivity(MyStats);
+    }
+
+    public void onClickView(View v) {
+        // go to the activity for displaying journeys
+        Intent myView = new Intent(TrophyCase.this, activityList.class);
+        startActivity(myView);
+    }
+
+
+
+    public void onClickMyTrophies(View v) {
+        // go to the activity for displaying statistics
+        Intent MyTrophies = new Intent(TrophyCase.this, neu.edu.madcourse.strideapp.TrophyCase.class);
+        startActivity(MyTrophies);
+    }
+
+    public void onClickRun(View v) {
+        Intent StartRun = new Intent(TrophyCase.this, neu.edu.madcourse.strideapp.MapsActivity.class);
+        startActivity(StartRun);
     }
 
 }
